0ed9bee5df169545156bf042a471660c46d44338
TalalSh <116476361+Talal-Sharaa@users.noreply.github.com>
2023-10-25 21:16:41 +0300

    Enhanced the searchfunctionality

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -4,10 +4,41 @@ const searchForm = document.getElementById('search-form');
     searchForm.addEventListener('submit', function(e) {
       e.preventDefault(); // Prevent form submission
 
-      const searchTerm = searchInput.value.trim();
+      const searchTerm = searchInput.value.trim().toLowerCase();
       if (searchTerm !== '') {
-        // Redirect to the search term page
-        const filename = searchTerm.toLowerCase().replace(/[^a-z0-9]/g, '') + '.html';
-        window.location.href = filename;
+        // Find the closest matching page name
+        const pageNames = [
+          'mcdonalds.html',
+          'burger-king.html',
+          'wendys.html',
+          // Add more page names as needed
+        ];
+
+        let closestMatch = '';
+        let closestMatchScore = -Infinity;
+
+        pageNames.forEach(pageName => {
+          const score = calculateMatchScore(searchTerm, pageName.toLowerCase());
+          if (score > closestMatchScore) {
+            closestMatch = pageName;
+            closestMatchScore = score;
+          }
+        });
+
+        // Redirect to the closest matching page
+        window.location.href = closestMatch;
       }
     });
+
+    function calculateMatchScore(searchTerm, pageName) {
+      let score = 0;
+
+      // Calculate the match score based on the similarity of characters
+      for (let i = 0; i < searchTerm.length; i++) {
+        if (pageName.includes(searchTerm[i])) {
+          score++;
+        }
+      }
+
+      return score;
+    }
