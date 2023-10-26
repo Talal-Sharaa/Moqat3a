97398bce3f7df93913072752042d029141b0655e
TalalSh <116476361+Talal-Sharaa@users.noreply.github.com>
2023-10-26 21:22:51 +0300

    AddedArabic Support for search & More companies Pages

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,45 +1,61 @@
-const searchForm = document.getElementById('search-form');
-    const searchInput = document.getElementById('search-input');
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
 
-    searchForm.addEventListener('submit', function(e) {
-      e.preventDefault(); // Prevent form submission
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent form submission
 
-      const searchTerm = searchInput.value.trim().toLowerCase();
-      if (searchTerm !== '') {
-        // Find the closest matching page name
-        const pageNames = [
-          'mcdonalds.html',
-          '',
-          '',
-          ''
-          // Add more page names as needed
-        ];
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  if (searchTerm !== "") {
+    // Find the closest matching page name
+    const pageNames = [
+      "mcdonalds-مكدونالدز.html",
+      "Bounty-Bens-Maars-whiskas-skittles-snickers-m&m-م&م-سكيتلز- سنيكرز-ويسكاس-باونتي-.html",
+      "always-gillette-olay-oral-crest-head&shoulders-pantine-duracell-braun-vicks-tide-downy-pampers-اولويز-بامبرز-جيليت-اولاي-اورال-هيد-اند-شولدرز-بانتين-دوراسل-براون-فيكس-تايد-داوني.html",
+      "Dove-Axe-Sunsilk-Vim-Close-Comfort-Lipton-Knorr-كنور-دوف-صن-سيلك-فيم-فازلين-vaseline-كومفورت-اكس-كلوز-ليبتون-tresemme-تريزيميه.html",
+      "Johnson&Johnson-جونسون.html",
+      "Garnier-Nestle-Nesquik-purina-kitkat-aero-smarties-ralph-giorgio-armani-nescafe-shop-شوب-بورينا-كيتكات-ايرو-نيسكافيه-نسكويك-loreal-لوريال-رالف-جارنييه-ميبيلين-maybeline.html",
+      "كرافت-هاينز-kraft-heinz.html",
+      "pizza-hut-بيتزا-هت.html",
+      "كلينكس-كلينيكس-kleenex.html",
+      "GM-chevrolet-cadillac-gmc-opel-شيفروليه-كاديلاك-جمس-اوبيل.html",
+      "Caterpillar-كاتربيلر.html",
+      "Hyundai-هيونداي.html",
+      "Pepsi-seven-marinda-dew-aquafinaa-أكوافينا-سيفن-بيبسي-ماريندا.html",
+      "Coca-cola-dew-sprite-fanta-monster-مونستر-سبرايت-فانتا-ديو-كولا.html"
+      // Add more page names as needed
+    ];
 
-        let closestMatch = '';
-        let closestMatchScore = -Infinity;
+    let closestMatch = "";
+    let closestMatchScore = -Infinity;
 
-        pageNames.forEach(pageName => {
-          const score = calculateMatchScore(searchTerm, pageName.toLowerCase());
-          if (score > closestMatchScore) {
-            closestMatch = pageName;
-            closestMatchScore = score;
-          }
-        });
-
-        // Redirect to the closest matching page
-        window.location.href = closestMatch;
+    pageNames.forEach((pageName) => {
+      const score = calculateMatchScore(searchTerm, pageName.toLowerCase());
+      if (score > closestMatchScore) {
+        closestMatch = pageName;
+        closestMatchScore = score;
       }
     });
 
-    function calculateMatchScore(searchTerm, pageName) {
-      let score = 0;
+    // Redirect to the closest matching page
+    window.location.href = closestMatch;
+  }
+});
 
-      // Calculate the match score based on the similarity of characters
-      for (let i = 0; i < searchTerm.length; i++) {
-        if (pageName.includes(searchTerm[i])) {
-          score++;
-        }
+function calculateMatchScore(searchTerm, pageName) {
+  let score = 0;
+  searchTerm = searchTerm.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  pageName = pageName.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  let searchTermIndex = 0;
+  for (let i = 0; i < pageName.length; i++) {
+    if (pageName[i] === searchTerm[searchTermIndex]) {
+      searchTermIndex++;
+      if (searchTermIndex === searchTerm.length) {
+        score += searchTerm.length; // Increase score by the length of searchTerm
+        break; // All letters in searchTerm have been found consecutively
       }
-
-      return score;
+    } else {
+      searchTermIndex = 0; // Reset searchTermIndex if consecutive match is broken
     }
+  }
+  return score;
+}
